230e08c903e522ff4ca5dde75c9a466a6c36287a
sandeep kamra <sandeep.d4d@gmail.com>
2025-03-18 15:10:07 +0530

    update the admin sidebar

--- src/components/admin/AdminHeader.js
+++ src/components/admin/AdminHeader.js
@@ -336,7 +336,7 @@ const AdminHeader = () => {
                       Team Account
                     </a>
                     <button
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
+                      className="w-full flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
                       onClick={handleLogout}
                     >
                       <svg
