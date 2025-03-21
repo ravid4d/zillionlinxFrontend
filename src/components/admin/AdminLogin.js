f6f06792e08bdfc8a61607297187c81e69c5cdff
Jaswant Singh <jaswant.d4d@gmail.com>
2025-03-21 15:43:59 +0530

    Fixed svg icon on admin login page

--- src/components/admin/AdminLogin.js
+++ src/components/admin/AdminLogin.js
@@ -38,7 +38,7 @@ const AdminLogin = () => {
     });
   return (
         <div className="flex flex-col bg-pattern bg-no-repeat bg-cover bg-center border shadow-sm rounded-[30px] pointer-events-auto w-full relative">
-             {/* <ToastContainer hideProgressBar={true} autoClose={2000} /> */}
+             <ToastContainer hideProgressBar={true} autoClose={2000} />
             <div className='w-full py-20 px-10'>
                 <div className="flex justify-between items-center">
                     <h3 id="hs-slide-down-animation-modal-label" className="uppercase text-dark-blue text-center w-full text-7xl mb-12">
